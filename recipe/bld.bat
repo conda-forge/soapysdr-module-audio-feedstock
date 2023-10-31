a0e68291ce86a8a082287cf8e6087e83e1709dd6
Ryan Volz <ryan.volz@gmail.com>
2023-10-31 17:20:11 -0400

    Add dep and set path to find pthread.h on Windows.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,6 +7,16 @@ if errorlevel 1 exit 1
 cd build
 if errorlevel 1 exit 1
 
+:: We need pthread headers for hamlib, but the ones that it built against
+:: (m2w64-winpthreads-git) can't be used in conjunction with MSVC. However,
+:: the newer mingw-w64 pthread headers contained in the winpthreads package
+:: *are* compatible, it's just that those haven't been rolled out with a new
+:: complete gcc build system on Windows yet. Moreover, it conflicts with the
+:: existing dependencies. But as a hack, we can install winpthreads in the build
+:: environment and point the compiler there to find pthread.h and company.
+set "CFLAGS=-external:I%BUILD_PREFIX%\Library\include"
+set "CXXFLAGS=-external:I%BUILD_PREFIX%\Library\include"
+
 :: configure
 cmake -G "Ninja" ^
     -DCMAKE_BUILD_TYPE:STRING=Release ^
